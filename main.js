612bab8ce3b750b4a4f380ace442e35c1bd746b6
MSuffiyan24 <msuffiyanpatni@gmail.com>
2024-07-20 00:28:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,470 @@
+// 45 QUESTION ASSIGNMENT //
+// QUESTION NO 1 
+// DOUNLOAD NODE.JS , VS CODE & TYPESCRIPT
+// QUESTION NO 2
+// let Name = 'Eric'
+// console.log(`Hello ${Name}, would you like to learn some Python today?`)
+// QUESTION NO 3
+// let Name: string = 'Suffiyan'
+// console.log(Name.toLowerCase())
+// console.log(Name.toUpperCase())
+// let Name1: string = 'suffiyan'
+// console.log(Name1.charAt(0).toUpperCase()+ Name1.slice(1))
+// QUESTION NO 4
+// console.log(`JRR Tolkien, "not all who wonder are lost"`)
+// QUESTION NO 5
+// let famous_Person = 'Wilhelm'
+// let massage = 'Reality is not what is used to be'
+// console.log(`${famous_Person} Once said, ${massage}`)
+// QUESTION NO 6
+// let Person_name = '\t Suffiyan \n'
+// console.log(`Person Name ${Person_name}`)
+// let clean = Person_name.trim()
+// console.log(clean)
+// QUESTION NO 7
+// let Add = 4+4
+// let Subtract = 15-7
+// let Multyply = 2*4
+// let divide = 32/4
+// console.log(Add)
+// console.log(Subtract)
+// console.log(Multyply)
+// console.log(divide)
+// QUESTION NO 8
+// console.log(5+3)
+// console.log(16-8)
+// console.log(2*4)
+// console.log(32/4)
+//QUESTION NO 9
+// let favorite_number = 99
+// console.log(`my favorite number is ${favorite_number}`)
+// QUESTION NO 10
+// THIS PROGRAM CONVEY MY MASSAGE AS SHOWN IN BELOW CODE //
+// let favorite_number = 99
+// console.log(`my favorite number is ${favorite_number}`)
+// QUESTION NO 11
+// let Names= ['Yaseen','Suffiyan','Arsalan']
+// console.log(Names[0])
+// console.log(Names[1])
+// console.log(Names[2])
+// QUESTION NO 12
+// let Names= ['Yaseen','Suffiyan','Arsalan']
+// console.log(`How are you ${Names[0]}`)
+// console.log(`How are you ${Names[1]}`)
+// console.log(`How are you ${Names[2]}`)
+// QUESTION NO 13
+// let Favorite_Vehicle= ['Suzuki_110','Suzuki Swift','Jeep Wrangler']
+// console.log(`I would like to own a  ${Favorite_Vehicle[0]}`)
+// console.log(`I would like to own a  ${Favorite_Vehicle[1]}`)
+// console.log(`I would like to own a  ${Favorite_Vehicle[2]}`)
+// QUESTION NO 14
+// let Guest = ['Yaseen','Muzzammil','Huzaifa']
+// console.log(`hey ${Guest[0]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[1]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[2]} would you like to come over for a dinner`)
+// QUESTION NO 15
+// console.log(`Unforuately for some reason ${Guest[2]} cant come`) 
+// Guest.pop()
+// Guest.push('Ikhlas')
+// console.log(Guest)
+// console.log(`hey ${Guest[0]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[1]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[2]} would you like to come over for a dinner`)
+// QUESTION NO 16
+// console.log('hey guys i just found a big table for a dinner')
+// Guest.unshift('Zeeshan')
+// Guest.splice(2,0,'Ali')
+// console.log(Guest)
+// console.log(`hey ${Guest[0]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[1]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[2]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[3]} would you like to come over for a dinner`)
+// console.log(`hey ${Guest[4]} would you like to come over for a dinner`)
+//QUESTION NO 17
+// console.log('Sorry Guys new table wont arrive on time now only two person can come')
+// Guest.pop()
+// Guest.pop()
+// Guest.pop()
+// console.log(Guest)
+// console.log(`${Guest} you two are invited`)
+// Guest.pop()
+// Guest.pop()
+// console.log(Guest)
+// QUESTION NO 18
+// let Countries = ['SaudiArabia','Íran','Japan','turkey','USA']
+//  console.log(Countries)
+//  console.log(Countries.sort())
+//  console.log(Countries)
+// console.log(Countries.reverse())
+// console.log(Countries)
+// let Places = ['SaudiArabia','Íran','Japan','turkey','USA']
+// console.log(Places)
+// console.log(Places.reverse())
+// console.log(Places.reverse())
+// Places.sort()
+// console.log(Places)
+// Places.reverse()
+// console.log(Places)
+// QUESTION NO 19
+// let Guest = ['Umair','Ali','Asad']
+// console.log(`${Guest.length} people are invited`)
+// QUESTION NO 20
+// let Mountain:string[] = ['k2','Everest','Himaliya']
+// console.log(Mountain)
+//QUESTION NO 21
+// interface RehmanBiryani {
+//     name: string;
+//     location: string;
+//     Quantity: number
+// }
+// let RehmanBiryani = {
+//     name:"Biryani",
+//     location: "Al-Rehman Biryani Near Tower Karachi",
+//     Quantity: 5
+// }
+// console.log(RehmanBiryani);
+//QUESTION NO 22
+//let Country_array: string[] = ['Yemen','Iran','japan','oman']
+//Showing Error: by inserting invalid index
+//console.log(Country_array[5]);
+// resolve Error
+//console.log(Country_array[3]);
+//QUESTION NO 23
+// let Two = 2;
+// let five = 5;
+// //test 1
+// console.log("test 1: two is equal to 2");
+// console.log( Two == 2);
+// //test 2
+// console.log("test 3: Five is equal to 5 ");
+// console.log( five == 5);
+// //test 3
+// console.log("test 3: two is not equal to 5");
+// console.log( Two !== 5);
+// //test 4
+// console.log("test 4: five is not equal to 2");
+// console.log( five !== 2);
+// //test 5
+// console.log("test 5: five is greater than 2");
+// console.log( five > 2);
+// //test 6
+// console.log("test 6: two is equal to 5");
+// console.log( Two == 5);
+// //test 7
+// console.log("test 7: Five is equal to 2");
+// console.log( five == 2);
+// //test 8
+// console.log("test 8: two is not equal to 2");
+// console.log( Two !== 2);
+// //test 9
+// console.log("test 9: five is  lower than 2");
+// console.log( five < 2);
+// //test 10
+// console.log("test 5: five is greater than 10");
+// console.log( five >10);
+//QUESTTION NO 24
+// let Fruit = "Apple";
+// //test  for  Equality
+// console.log("Apple is equal to Apple");
+// console.log(Fruit == 'Apple');
+// //test  for  Inequality
+// console.log("Apple is not equal to Apple");
+// console.log(Fruit != 'Apple');
+// // test using Lowercasefunctionn
+//  let UppercaseApple = 'APPLE'
+//  //Equal to
+// console.log('APPLE is Equal to apple by converting into lowercase');
+// console.log(UppercaseApple.toLocaleLowerCase() == "apple");
+// // Inequal to
+// console.log('APPLE is not Equal to apple by converting into lowercase');
+// console.log(UppercaseApple.toLocaleLowerCase() != "apple");
+// test Using  Equal, Not Equal , Less Than Equal to , Greater than  equal to
+// let Ten = 10;
+// let twenty = 20;
+// // Equal  to
+// console.log("Ten is equal to 20");
+// console.log(Ten == 20);
+// // Not Equal  to
+// console.log("Ten is not equal to 20");
+// console.log(Ten != 20);
+// // Less than or equal to
+// console.log("twenty is less than or equal to 10");
+// console.log(twenty <= 10);
+// // Greater than or equal to
+// console.log("twenty is greater than or equal to 10");
+// console.log(twenty >= 10);
+// // Less than
+// console.log("twenty is less than 10");
+// console.log(twenty < 10);
+// // Greater than
+// console.log("twenty is greater than 10");
+// console.log(twenty > 10);
+//  test Using And and or operator
+//And Operator &&
+// console.log("twenty is not equal to ten and twenty is greater than ten");
+// console.log(twenty != 10 && twenty > 10 );
+// console.log("twenty is not equal to ten and ten is greater than twenty");
+// console.log(twenty != 10 && Ten > 20 );
+// // Or Operator ||
+// console.log("twenty is not equal to ten or twenty is greater than ten");
+// console.log(twenty != 10 || twenty > 10 );
+// console.log("twenty is equal to ten and ten is greater than twenty");
+// console.log(twenty == 10 || Ten > 20 );
+//Test whether an array item is in a array
+// let fruits = ["Orange", "Bannana","Mango"]
+// console.log("Orange fruits is include in Array");
+// console.log(fruits.includes("Orange"));
+// //Test whether an item not is in an array
+// console.log("Orange fruits is not include in Array");
+// console.log(!fruits.includes("Orange"));
+//QUESTION NO 25
+// let alien_colour = "green"
+// if ( alien_colour === "green") { 
+//     console.log('player just earned 5 Points');
+// } 
+// if ( alien_colour === "white"){
+//     console.log("you guess the wrong colour");   
+// }
+//QUESTION NO 26
+// first statement 
+// let Alien_Color = "Green"
+// if ( Alien_Color === "Green") { 
+//          console.log('Alien Colour is green. player just earned 5 Points for shooting alien.');
+//      } 
+//      if ( Alien_Color === "white"){
+//          console.log("you guess the wrong colour of alien");   
+//      }
+// Second statement 
+// let alien_Color = "Red"
+// if ( alien_Color === "green") { 
+//          console.log('Alien Colour is green. player just earned 5 Points for shooting alien.');
+//      } 
+//      if ( alien_Color === "Red"){
+//          console.log('Alien Colour is not green. player just earned 10 Points for shooting alien.'); 
+//      }
+//QUESTION NO 27
+//VERSOPN 1
+//let Alien_Color = "Green"
+// if ( Alien_Color === "Green") { 
+//          console.log('(Version 1) Alien Colour is green. player just earned 5 Points.');
+//      } 
+//      if ( Alien_Color === "white"){
+//          console.log("you guess the wrong colour of alien");   
+//      }
+//VERSOPN 2
+// let Alien_Color = "Red"
+// if ( Alien_Color === "Red") { 
+//          console.log('(Version 2) Alien Colour is Red. player just earned 15 Points.');
+//      } 
+//      if ( Alien_Color === "white"){
+//          console.log("you guess the wrong colour of alien");   
+//      }
+// // //VERSOPN 3
+// let Alien_Color = "Yellow"
+// if ( Alien_Color === "Yellow") { 
+//          console.log('(Version 3) Alien Colour is yellow. player just earned 10 Points.');
+//      } 
+//      if ( Alien_Color === "white"){
+//          console.log("you guess the wrong colour of alien");   
+//      }
+//QUESTION NO 28
+// let age = 24;
+// if (age < 2){
+//     console.log("its a baby");
+// } 
+// else if(age >= 2  && age < 4) {
+//  console.log("its a toddler");
+// }
+// else if(age >= 4  && age < 13) {
+//     console.log("its a Kid");
+// }
+// else if(age >= 13  && age < 20) {
+//     console.log("its a teenager");
+// }
+// else if(age >= 20  && age < 65) {
+//     console.log("this person is an adlut");
+// }
+// else if(age >= 65) {
+//     console.log("this person is am Elder");
+// }
+//QUESTION NO 29
+// let Favorite_fruits = ["Mango","Apple","Cherry"];
+// if (Favorite_fruits.includes("Mango")) {
+//     console.log("i really like mango ");   
+// }
+// if (Favorite_fruits.includes("Apple")) {
+//     console.log("i really like Apple ");   
+// }
+// if (Favorite_fruits.includes("Cherry")) {
+//     console.log("i really like Cherry");   
+// }
+// if (Favorite_fruits.includes("Bannana")) {
+//     console.log("i really like Bannana");   
+// }
+// if (Favorite_fruits.includes("Orange")) {
+//     console.log("i really like Orange ");   
+// }
+//QUESTION NO 30
+// let UserName = ["Suffiyan","Admin","Ali","Ashraf","Hamza"]
+// UserName.forEach(oneuser => {
+//     if (oneuser === "Admin"){
+//         console.log(`Hello ${oneuser}, would you like to see status report?`);
+//     }else {
+//         console.log(`hey! ${oneuser} thank you for logging again`);
+//     }
+// })
+//QUESTION NO  31
+// let UserName = ["Suffiyan","Admin","Ali","Ashraf","Hamza"];
+// UserName = []
+// if(UserName.length === 0){
+//   console.log("we need to find some  user");
+// } else {
+//     UserName.forEach(oneuser => {
+//             if (oneuser === "Admin"){
+//                 console.log(`Hello ${oneuser}, would you like to see status report?`);
+//             }else {
+//                 console.log(`hey! ${oneuser} thank you for logging again`);
+//             }
+//         })
+// }
+// QUESTION NO 32
+// let Current_User = ["Arsalan","Yaseen","Zeeshan","ali","bilal"]
+// let New_User = ["Suffiyan","Qasim","ali","bilal","Arif" ]
+// New_User.forEach(Userdata => {
+//     let User_condition = Current_User.some(Userlist => Userlist.toLocaleLowerCase() === Userdata.toLocaleLowerCase())
+//     if (User_condition){
+//         console.log(`Sorry this username ${Userdata} has already taken`);
+//     } else {
+//         console.log(`this username ${Userdata} is available`);
+//     }
+// })
+// QUESTION NO 33
+// let numbers = [1,2,3,4,5,6,7,8,9];
+// for (let Onenumbers of numbers){
+//     let ending:string;
+//     if(Onenumbers === 1){
+//        ending = "st"
+//     }else if(Onenumbers === 2){
+//         ending = "nd"
+//     }else if(Onenumbers === 3){
+//         ending = "rd"
+//     }else {ending = "th"}
+//     console.log(`${Onenumbers}${ending}`);
+// }
+// QUESTION NO 34
+// let Pizzas = ["Tikka","BBQ","Malaitikka"]
+// for (let AllPizzas of Pizzas){
+//         console.log(`I really Love eating ${AllPizzas} Flavour Pizza`);
+// }
+// console.log("I love all kind of pizzas");
+// QUESTION NO 35
+// let Animal = ["Parrot","Dog","Cat"]
+// for(let Pet of Animal){
+//  console.log(`A ${Pet} would make a great pet`);
+// }
+//  console.log("any of this animal would be great as a pet");
+// QUESTION NO 36
+// function make_Shirt (Size:string, Massage:string){
+//    console.log(`Your Shirt is ${Size} Size and Print on shirt is ${Massage}`);
+// }
+// make_Shirt('Large','Belive in your self')
+//QUESTION NO 37
+// function make_Shirt (Size:string = "Large" , Massage:string = "I love typescript"){
+//       console.log(`Making a ${Size} size shirt having ${Massage} massage printed on it`);
+// }
+// make_Shirt()
+// make_Shirt("Medium")
+// make_Shirt("Small", "I Love Generative AI")
+// QUESTION NO 38
+// function describe_City (City:string, Country:string = "Pakistan"){
+//       console.log(`${City} is in ${Country}`);
+// }
+// describe_City("Karachi")
+// describe_City("Lahore")
+// describe_City("Mumbai","India")
+// QUESTION NO 39
+// function city_country (city:string, Country:string):string{
+//      return `${city},${Country}`
+// }
+// console.log(city_country("Karachi","Pakistan"));
+// console.log(city_country("shanghai","china"));
+// console.log(city_country("Paris","france"));
+//QUESTION NO 40
+// function make_album (artist_name:string, Tittle_name:string, tracks?:number){
+//     let album: {artist:string, Tittle:string, tracks?:number} = {
+//         artist: artist_name,
+//         Tittle: Tittle_name
+//     }
+//     if(tracks !== undefined){
+//        album.tracks = tracks
+//     }
+//     return album;
+// }
+// let album1 = make_album("Atif", "Album A")
+// let album2 = make_album("Ali", "Album B")
+// let album3 = make_album("rahat", "Album C", 5)
+// console.log(album1);
+// console.log(album2);
+// console.log(album3);
+//QUESTION NO 41
+// function show_magicians (magicians:string[]){
+//     magicians.forEach(names => (console.log(names)
+//     ) )
+// }
+// let magicians_name = ["magician A","magician B","magician C"]
+// show_magicians(magicians_name)
+//QUESTION NO 42
+// function show_magicians (magicians:string[]){
+//     magicians.forEach(names => (console.log(names)
+//     ) )
+// }
+// function make_great (magicians:string[]){
+//    return magicians.map( name => `the great ${name}`)
+// }
+// let magicians_name = ["magician A","magician B","magician C"]
+// let great_magicians = make_great(magicians_name)
+// show_magicians(great_magicians)
+//QUESTION NO 43 
+// function show_magicians (magicians:string[]){
+//     magicians.forEach(names => (console.log(names)
+//     ) )
+// }
+// function make_great (magicians:string[]){
+//   return magicians.map( name => `the great ${name}`)
+// }
+// let magicians_name = ["magician A","magician B","magician C"]
+// let copy_magicians_name = magicians_name.slice();
+// let copy_make_great = make_great(copy_magicians_name)    
+// console.log("\nORIGINAL ARREY\n");
+// show_magicians(magicians_name)
+// console.log("\nCOPIED ARRAY\n");
+// show_magicians(copy_make_great)
+//QUESTION NO 44
+// function sandwitch (...items:string[]){
+//     console.log('\nwe are making an sandwitch with following items');
+//     items.forEach(list => (
+//         console.log(list)
+//     ))
+//     console.log("\nenjoy your Meal");
+// }
+// sandwitch('Bread','cheese','Butter')
+// sandwitch('Beaf','Tomatos')
+// sandwitch('Salad','Egg','sauce','mashroom')
+//QUESTION NO 45
+function create_car(manufacturer, Model) {
+    var Option = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        Option[_i - 2] = arguments[_i];
+    }
+    var car = {
+        manufacturer: manufacturer,
+        Model: Model
+    };
+    Option.forEach(function (option) {
+        var _a = option.split(":"), key = _a[0], value = _a[1];
+        car[key.trim()] = value.trim();
+    });
+    return car;
+}
+var my_car = create_car("Toyota", "Corolla GLI", "Color:Black", "Condition:10/10");
+console.log(my_car);
